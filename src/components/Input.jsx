824e781b95b77aa93fda725c33954d894fa600a6
GeraAnggaraPutra <gera_098@smkassalaambandung.sch.id>
2023-04-20 10:16:15 +0700

    add messages and input send messages

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import Img from '../img/img.png'
+import Attach from '../img/attach.png'
 
 export const Input = () => {
   return (
-    <div>Input</div>
+    <div className='input'>
+      <input type="text" placeholder='Type something...' />
+      <div className="send">
+        <img src={Img} alt="" />
+        <input type="file" style={{ display: 'none' }} id='file' />
+        <label htmlFor="file">
+          <img src={Attach} alt="" />
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
   )
 }
---
+++ src/components/Messages.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Message} from './Message'
+export const Messages = () => {
+  return (
+      <div className='messages'>
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+    </div>
+  )
+}
