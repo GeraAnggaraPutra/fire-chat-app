5d59da0b4b75e3170e4b4d7c4156930f40b0edb2
GeraAnggaraPutra <gera_098@smkassalaambandung.sch.id>
2023-04-20 10:46:23 +0700

    style messages

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export const Message = () => {
   return (
-    <div>Message</div>
-  )
+    <div className="message owner">
+      <div className="message-info">
+        <img src="http://placeimg.com/640/480/people" alt="" />
+        <span>just now</span>
+      </div>
+      <div className="message-content">
+        <p>hello</p>
+        <img src="http://placeimg.com/640/480/people" alt="" />
+      </div>
+    </div>
+  );
 }
