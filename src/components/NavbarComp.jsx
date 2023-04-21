6972564e364c1544795b877ebd4c1c6c1645a669
GeraAnggaraPutra <gera_098@smkassalaambandung.sch.id>
2023-04-21 09:32:07 +0700

    display avatar and name user in navbar

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { auth } from '../firebase';
 import { signOut } from 'firebase/auth';
+import { AuthContext } from '../context/AuthContext';
 
 export const NavbarComp = () => {
+
+  const {currentUser} = useContext(AuthContext)
   return (
     <div className="navbar">
       <span className="logo">Fire Chat</span>
       <div className="user">
-        <img src="http://placeimg.com/640/480/people" alt="" />
-        <span>john</span>
+        <img src={currentUser.photoURL} alt="" />
+        <span>{ currentUser.displayName }</span>
         <button onClick={()=>signOut(auth)}>Logout</button>
       </div>
     </div>
