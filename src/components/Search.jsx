1b382f169c268298671750cb7df2f7c47e338722
GeraAnggaraPutra <gera_098@smkassalaambandung.sch.id>
2023-04-21 10:13:57 +0700

    click user in search and create chats

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,15 @@
 import React, { useContext, useState } from "react";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  setDoc,
+  doc,
+  updateDoc,
+  serverTimestamp,
+  getDoc,
+} from "firebase/firestore";
 import { db } from "../firebase";
 import { AuthContext } from "../context/AuthContext";
 
@@ -29,9 +39,43 @@ export const Search = () => {
     e.code === "Enter" && handleSearch();
   };
 
-  const handleSelect = () => {
+  const handleSelect = async () => {
     //check whether the group(chats in firestore) exists, if not create
-  }
+    const combinedId =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+    try {
+      const res = await getDoc(doc(db, "chats", combinedId));
+
+      if (!res.exists()) {
+        //create a chat in chats collection
+        await setDoc(doc(db, "chats", combinedId), { messages: [] });
+
+        //create user chats
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+
+        await updateDoc(doc(db, "userChats", user.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+      }
+    } catch (err) {}
+
+    setUser(null);
+    setUsername("");
+  };
   return (
     <div className="search">
       <div className="search-form">
