efb26cb9b77d5dce2e08f1479cdea8bd54dba225
GeraAnggaraPutra <gera_098@smkassalaambandung.sch.id>
2023-04-21 17:15:44 +0700

    chat use context display name other user

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Cam from '../img/cam.png'
 import Add from '../img/add.png'
 import More from '../img/more.png'
 import { Messages } from './Messages'
 import {Input} from './Input'
+import { ChatContext } from '../context/ChatContext'
 
 export const Chat = () => {
+  const { data } = useContext(ChatContext);
   return (
-    <div className='chat'>
+    <div className="chat">
       <div className="chat-info">
-        <span>Gerd</span>
+        <span>{data.user?.displayName}</span>
         <div className="chat-icons">
           <img src={Cam} alt="" />
           <img src={Add} alt="" />
@@ -19,5 +21,5 @@ export const Chat = () => {
       <Messages />
       <Input />
     </div>
-  )
+  );
 }
