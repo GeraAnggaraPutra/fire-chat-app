18e847ea1781af4498bb1ae98608cf3fe26dd9ac
GeraAnggaraPutra <gera_098@smkassalaambandung.sch.id>
2023-04-21 09:13:18 +0700

    protected route

--- src/App.js
+++ src/App.js
@@ -3,22 +3,34 @@ import './App.css';
 import { Home } from './pages/Home';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthContext } from './context/AuthContext';
 
 function App() {
 
   const { currentUser } = useContext(AuthContext)
-  console.log(currentUser)
+  
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />
+    }
+  }
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/'>
-          <Route index element={<Home/>} />
-          <Route path='login' element={<Login/>} />
-          <Route path='register' element={<Register/>} />
+        <Route path="/">
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
         </Route>
-    </Routes>
+      </Routes>
     </BrowserRouter>
   );
 }
