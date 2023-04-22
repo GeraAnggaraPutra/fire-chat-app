14eb322c08f30fa97a0acd7fe2e8964974959577
GeraAnggaraPutra <gera_098@smkassalaambandung.sch.id>
2023-04-22 19:15:49 +0700

    fix timestamp

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
 
@@ -7,8 +7,18 @@ const Message = ({ message }) => {
   const { data } = useContext(ChatContext);
 
   const ref = useRef();
+  const [time, setTime] = useState("");
 
   useEffect(() => {
+    const messageTime = new Date(
+      message.date.seconds * 1000 + message.date.nanoseconds / 1000000
+    );
+    const messageTimeString = messageTime.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+    setTime(messageTimeString);
+
     ref.current?.scrollIntoView({ behavior: "smooth" });
   }, [message]);
 
@@ -26,7 +36,7 @@ const Message = ({ message }) => {
           }
           alt=""
         />
-        <span>just now</span>
+        <span>{time || "just now"}</span>
       </div>
       <div className="message-content">
         <p>{message.text}</p>
